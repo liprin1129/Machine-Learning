7a4b754165eb7b1c172427abe282c80207622fb8
SeongMuk Gang <>
2018-03-14 00:12:34 +0900

    mnist cnn

--- Word_Recognition/mnist_CNN.py
+++ Word_Recognition/mnist_CNN.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 global_path = os.path.abspath("/mnt/SharedData/Development/Personal_Dev/Machine-Learning/One_Layer/")
+global_path = os.path.abspath("../One_Layer/")
 
 if global_path not in sys.path:
     sys.path.append(global_path)
@@ -12,6 +13,9 @@ from pickle_helper import PickleHelper
 
 import numpy as np
 from tqdm import tqdm
+
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
 import tensorflow as tf
@@ -20,8 +24,8 @@ from tensorflow.contrib.layers import flatten
 #from tensorflow.python.framework import ops
 
 # START A GRAPH SESSION
-sess = tf.Session()
-
+#sess = tf.Session()
+#sess = tf.InteractiveSession()
 # DOWNLOAD DATA
 '''
 font_data = PickleHelper.load_pickle(
@@ -47,13 +51,23 @@ test_labels = mnist.test.labels
 # MODEL PARAMETERS
 img_channel = 1
 learning_rate = 0.001
-train_steps = 20000
+epoch = 20000
 batch_size = 100
+drop_prop = 0.4
 
 # PLACEHOLDERS
 img_shape = (None, 28, 28, img_channel)
-in_true = tf.placeholder(tf.float32, shape=img_shape)
-out_true = tf.placeholder(tf.int32, shape=(None))
+x = tf.placeholder(tf.float32, shape=img_shape)
+
+labels = tf.placeholder(tf.int32, shape=(None))
+y_ = tf.one_hot(labels, depth=10, dtype=tf.float32)
+
+keep_prob = tf.placeholder(tf.float32)
+'''
+with tf.Session() as sess:
+    a = sess.run(y_, feed_dict={labels:train_labels})
+    print(np.shape(a))
+''' 
 
 eval_in_layer = tf.placeholder(tf.float32, shape=img_shape)
 eval_out_layer = tf.placeholder(tf.int32, shape=(None))
@@ -146,7 +160,7 @@ def cnn_dense_model(features):
     flatten_node = flatten(max_pool2)
 
     dense1 = tf.nn.relu(tf.add(tf.matmul(flatten_node, dense1_W), dense1_b))
-    dropout1 = tf.nn.dropout(dense1, drop_prop)
+    dropout1 = tf.nn.dropout(dense1, keep_prob)
 
     # DENS2 -> LOGITS
     logits = tf.nn.relu(tf.add(tf.matmul(dropout1, dense2_W), dense2_b))
@@ -154,20 +168,39 @@ def cnn_dense_model(features):
     return logits
 
 # CALCULATE LOSS (for both TRAIN and EVAL modes)
-logits = cnn_dense_model(in_true)
-loss = tf.reduce_mean(
-    tf.nn.sparse_softmax_cross_entropy_with_logits(
-        logits=logits, labels=out_true))
+logits = cnn_dense_model(x)
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(
+        logits=logits, labels=y_))
 
-prediction = tf.nn.softmax(logits, name='softmax_tensor')
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+train_step = optimizer.minimize(loss=cross_entropy)                                
 
-def cnn_dense_train(loss):
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
-    train_optimizer = optimizer.minimize(
-        loss=loss,
-        global_step=tf.train.get_global_step())
-    return train_optimizer
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    one_hot_train_labels = sess.run(y_, feed_dict={labels:train_labels})
+    
+    for i in range(epoch):
+        batch_index = np.random.choice(len(train_xdata), size=batch_size)
+        batch_x = np.expand_dims(train_xdata[batch_index], axis=-1)
+        batch_y = one_hot_train_labels[batch_index]
+
+        if i % 100 == 0:
+            train_accuracy = accuracy.eval(feed_dict={
+                x: batch_x, y_: batch_y, keep_prob: 1.0})
+            #train_accuracy = sess.run(accuracy, feed_dict={
+            #    x: batch_x, y_: batch_y, keep_prob: 1.0})
+            print('step %d, training accuracy %g' % (i, train_accuracy))
+        train_step.run(feed_dict={x: batch_x, y_: batch_y, keep_prob: drop_prop})
+        #sess.run(train_step, feed_dict={x: batch_x, y_: batch_y, keep_prob: drop_prop})
+
+        #print('test accuracy %g' % accuracy.eval(feed_dict={
+        #    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
+"""
 def cnn_dense_evaluate(logits, labels):
         pred = np.argmax(logits, axis=1)
         #np.argmax(tf.nn.softmax(logits, name='softmax_tensor'), axis=1)
@@ -176,10 +209,9 @@ def cnn_dense_evaluate(logits, labels):
         
 
 # INITIALIZE
-init = tf.global_variables_initializer()
-sess.run(init)
+sess.run(tf.global_variables_initializer())
 
-for i in tqdm(range(train_steps)):
+for i in tqdm(range(epoch)):
     rand_index = np.random.choice(len(train_xdata), size=batch_size)
     batch_x = np.expand_dims(train_xdata[rand_index], axis=-1)
     batch_y = train_labels[rand_index]
@@ -207,11 +239,12 @@ for i in tqdm(range(train_steps)):
         print(sess.run(evaluate_stage, train_dict))
     '''
 
-"""
+'''
 train_xdata = np.expand_dims(train_xdata[10], -1)
 train_xdata = np.expand_dims(train_xdata, 0)
 train_ydata = train_labels[10]
 
 logits = cnn_dense_model(in_true, out_true, 'test')
 print(sess.run(logits, feed_dict={in_true: train_xdata, out_true: train_ydata}))
+'''
 """
