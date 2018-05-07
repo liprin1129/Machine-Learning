22da88c3599ef20b9017706f3ee5722120e9f9b4
Seongmuk Gang <liprin1129@gmail.com>
2018-05-07 17:27:39 +0900

    VGG16 in progress

--- TensorFlow/face_classifier/data_wrangling.py
+++ TensorFlow/face_classifier/data_wrangling.py
@@ -73,8 +73,11 @@ class DataWrangling(object):
                     #jpg_list_with_path.extend([os.path.join(path, img_file)]) #jpg_list_with_path.extend([os.path.join(path, x) for x in fnames if rx.search(x)])
                     img_cv = self.read_img_with_abs_path(os.path.join(path, img_file)) 
                     if img_cv is not None:
+                        img_cv = self.bgr2gray(img_cv) # convert color to gray scale
+                        img_cv = self.resize_img(img_cv, (32, 32)) # resize the image
+                        self.scailing(img_cv, new_min=0, new_max=255) # rescailing
+                        #print(np.shape(img_cv), np.min(img_cv), np.max(img_cv))
                         features.append(img_cv)
-                        
                     #jpg_list_without_path.append(img_file[:-9])
                     labels.append(img_file[:-9])
 
@@ -91,15 +94,36 @@ class DataWrangling(object):
             self._img = cv2.imread(self._folder_path + file_name, cv2.IMREAD_COLOR)
         #self._folder_path = folder_path
         '''
-        
+
+    def bgr2gray(self, image):
+        return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    def resize_img(self, img, size=(32, 32)):
+        return cv2.resize(img, dsize=size, interpolation = cv2.INTER_AREA)
+
+    def scailing(self, img, new_min = 0, new_max = 255):
+        '''
+        old_max = np.max(img)
+        old_min = np.min(img)
+
+        return ((new_max - new_min) / (old_max - old_min)) * (img - old_min) + new_min
+        '''
+        cv2.normalize(img, dst=img, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=-1)
+
+        #return img
+
+   
 if __name__=="__main__":
-    data_dir =  "/Users/pure/Developments/Personal-Study/Machine-Learning/Data/Face-Data/"
-    '''
+    #data_dir =  "/Users/pure/Developments/Personal-Study/Machine-Learning/Data/Face-Data/"
+    data_dir = "../../Data/"
+    # data wrangling and save to pickle
     data_wrangling = DataWrangling(data_dir)
     features, labels = data_wrangling.img2numpy_array(data_wrangling.dir_path)
 
     PickleHelper.save_to_pickle(data_dir, "faces-features.pkl", features)
     PickleHelper.save_to_pickle(data_dir, "faces-labels.pkl", labels)
-    '''
+
+    ''' # Load Data
     features = PickleHelper.load_pickle(data_dir, "faces-features.pkl")
     print(np.shape(features))
+    '''
--- TensorFlow/face_classifier/face_classifier.py
+++ TensorFlow/face_classifier/face_classifier.py
@@ -1,25 +1,42 @@
-import numpy as np
-import tensorflow as tf
-
-from tqdm import tqdm
-
 import platform
-
-if platform.system() is "Darwin":
-	import matplotlib
-	matplotlib.use('TkAgg')
-	import matplotlib.pyplot as plt
+if platform.system() == "Darwin":
+    print("Platform is", platform.system())
+    import matplotlib
+    matplotlib.use("TkAgg")
+    import matplotlib.pyplot as plt
 else:
-	import matplotlib.pyplot as plt
+    import matplotlib.pyplot as plt
+
+import numpy as np
 
 import cv2
-
+import tensorflow as tf
 from tensorflow.contrib.layers import flatten
 
+from data_wrangling import PickleHelper
+
 class Face_Classifier_With_Tensorflow(object):
-	def __init__(self):
+    def __init__(self):
+        self.__path = None
+
+    def imshow(self, img):
+        plt.imshow(img[...,::-1], cmap="gray")
+        plt.show()
 
 def face_classifier_did_loaded():
-	print("Hello, World!")
+    
+    fc = Face_Classifier_With_Tensorflow()
 
-plt.show()
+    img_cv = PickleHelper.load_pickle("../../Data/", "faces-features.pkl")
+
+    print(np.shape(img_cv))
+    fc.imshow(img_cv[0])
+
+    '''
+    img = cv2.imread("../../Data/lfw/Aaron_Eckhart/Aaron_Eckhart_0001.jpg")
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    img2 = cv2.resize(img, dsize=(32,32), interpolation=cv2.INTER_LINEAR_EXACT)
+    #fc.imshow(img)
+    fc.imshow(img2)
+    '''
