00ba7c5d6ab4ed9445030e29b1b7dba16fba878c
Seongmuk Gang <liprin1129@gmail.com>
2018-05-18 17:14:12 +0900

    take video stream in progress

---
+++ TensorFlow/face_data_wrangling/sjcFaceCollector/sjcFaceCollector/mainDelegate.hpp
@@ -0,0 +1,20 @@
+//
+//  mainDelegate.hpp
+//  sjcFaceCollector
+//
+//  Created by 170 on 2018/05/18.
+//  Copyright © 2018 170. All rights reserved.
+//
+
+#ifndef mainDelegate_hpp
+#define mainDelegate_hpp
+
+#include "imageManager.hpp"
+
+class MainDelegate {
+	
+public:
+	auto mainDelegation(int argc, char** argv) -> int;
+};
+
+#endif /* mainDelegate_hpp */
