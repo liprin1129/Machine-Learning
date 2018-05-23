6e33d65d5e86939a43a273c86ebeea5c6d44f4c4
Unknown <liprin1129@gmail.com>
2018-05-23 15:36:24 +0900

    Auto stash before revert of "face detection doesn't properly work"

--- TensorFlow/face_classifier/Refactoring/simple_CNN_refactoring.py
+++ TensorFlow/face_classifier/Refactoring/simple_CNN_refactoring.py
@@ -28,23 +28,6 @@ class CNN(netParam):
         self._pred_nd = None
         self._acc_nd = None
 
-        # Network shapes
-        self.kernel_1x1 = 1
-        self.kernel_3x3 = 3
-        
-        self.depth1 = 12
-        self.depth2 = 24
-        self.depth3 = 48
-        self.depth4 = 96
-        self.depth5 = 192
-
-        self.dense_4096 = 4096
-        self.dense_1000 = 1000
-        self.dense_output = 2
-
-        self.stride = 1
-
-
     def _convolutional_layer(self, in_node, kernel_size, out_depth, stride, padding, name):
         with tf.variable_scope(name):
             conv_filter = self._filter_var(
@@ -72,82 +55,52 @@ class CNN(netParam):
         else:
             return tf.add(tf.matmul(in_node, dense_weight), dense_bias)
 
-    def simple_CNN(self, input_node):
+    def architecture(self, input_node):
         # CONVOLUTIONAL 1
+        '''
+        conv1_W = tf.Variable(
+            tf.truncated_normal([3, 3, 3, 32], stddev=0.1, dtype=tf.float32))
+        conv1_b = tf.Variable(tf.truncated_normal([32], stddev=0.1, dtype=tf.float32))
+        
+        conv1 = tf.layers.conv2d(inputs=input_node, filters=32, kernel_size=[3, 3], strides=(1, 1),  padding="valid", activation=tf.nn.relu)
+        '''
         conv1 = self._convolutional_layer(input_node, 3, 32, 1, "VALID", "conv1")
+        #conv1 = tf.nn.conv2d(feature_node, conv1_W, strides=[1, 1, 1, 1], padding='VALID')
+        #relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_b))
+        #pool1 = tf.layers.max_pooling2d(inputs = conv1, pool_size = [2,2], strides = 2)
         
         # FLATTEN
         relu1_shape = conv1.get_shape().as_list()
         conv1_flat = tf.reshape(conv1, [-1, relu1_shape[1]*relu1_shape[2]*relu1_shape[3]])
         
         # DENSE 1
+        #dense1 = tf.layers.dense(inputs=conv1_flat, units=128, activation=tf.nn.relu)#tf.add(tf.matmul(
         dense1 = self._dense_layer(conv1_flat, 128, "relu", "dense1")
-
         # DENSE 2
+        #dense2 = tf.layers.dense(inputs=dense1, units=2)#tf.add(tf.matmul(
         dense2 = self._dense_layer(dense1, 2, "None", "dense2")
         
         self._logits_nd = dense2
 
-    def VGG16(self, input_node):
-        conv1_1 = self._convolutional_layer(input_node, self.kernel_3x3, self.depth1, self.stride, "SAME", "conv1_1")
-        conv1_2 = self._convolutional_layer(conv1_1, self.kernel_3x3, self.depth1, self.stride, "SAME", "conv1_2")
-        max_pool1 = self._max_pool(conv1_2, "max_pool1")
-
-        conv2_1 = self._convolutional_layer(max_pool1, self.kernel_3x3, self.depth2, self.stride, "SAME", "conv2_1")
-        conv2_2 = self._convolutional_layer(conv2_1, self.kernel_3x3, self.depth2, self.stride, "SAME", "conv2_2")
-        max_pool2 = self._max_pool(conv2_2, "max_pool2")
-
-        conv3_1 = self._convolutional_layer(max_pool2, self.kernel_3x3, self.depth3, self.stride, "SAME", "conv3_1")
-        conv3_2 = self._convolutional_layer(conv3_1, self.kernel_3x3, self.depth3, self.stride, "SAME", "conv3_2")
-        conv3_3 = self._convolutional_layer(conv3_2, self.kernel_1x1, self.depth3, self.stride, "SAME", "conv3_3")
-        max_pool3 = self._max_pool(conv3_3, "max_pool3")
-
-        conv4_1 = self._convolutional_layer(max_pool3, self.kernel_3x3, self.depth4, self.stride, "SAME", "conv4_1")
-        conv4_2 = self._convolutional_layer(conv4_1, self.kernel_3x3, self.depth4, self.stride, "SAME", "conv4_2")
-        conv4_3 = self._convolutional_layer(conv4_2, self.kernel_1x1, self.depth4, self.stride, "SAME", "conv4_3")
-        max_pool4 = self._max_pool(conv4_3, "max_pool4")
-
-        conv5_1 = self._convolutional_layer(max_pool4, self.kernel_3x3, self.depth5, self.stride, "SAME", "conv5_1")
-        conv5_2 = self._convolutional_layer(conv2_1, self.kernel_3x3, self.depth5, self.stride, "SAME", "conv5_2")
-        conv5_3 = self._convolutional_layer(conv2_2, self.kernel_1x1, self.depth5, self.stride, "SAME", "conv5_3")
-        max_pool5 = self._max_pool(conv5_3, "max_pool5")
-
-        # FLATTEN
-        relu1_shape = max_pool5.get_shape().as_list()
-        max_pool5_flat = tf.reshape(max_pool5, [-1, relu1_shape[1]*relu1_shape[2]*relu1_shape[3]])
-        
-        #print(max_pool5_flatten.get_shape())
-        dense1 = self._dense_layer(max_pool5_flat, self.dense_4096, "relu", name="dense1")
-        dense2 = self._dense_layer(dense1, self.dense_4096, "relu", name="dense2")
-        dense3 = self._dense_layer(dense2, self.dense_1000, "relu", name="dense3")
-        dense_output = self._dense_layer(dense3, self.dense_output, None, name="dense_output")
-        #dense_output = tf.nn.dropout(dense_output, keep_prob_node)
-        # Dens -> Probabilities
-        #probabilities = tf.nn.softmax(dense3, name="probabilities")
-        #self.__architecture = probabilities
-
-        # Dens -> Logits
-        #logits = tf.nn.relu(dense_output)
-        self._logits_nd = dense_output
-
-    def _cost_function(self):
+    def cost_function(self):
         # CONST FUNCTION
         self._loss_nd = tf.losses.softmax_cross_entropy(onehot_labels=self._label_nd, logits=self._logits_nd)
 
-    def _optimizer(self):
+    def optimizer(self):
         # OPTIMIZER
         optimizer = tf.train.AdamOptimizer(learning_rate=self._rl)
         train_op = optimizer.minimize(loss=self._loss_nd, global_step=tf.train.get_global_step())
         self._train_optimizer_nd = train_op
 
-    def _prediction(self):
+    def prediction(self):
         # PREDICTION
         self._pred_nd = tf.argmax(self._logits_nd, 1)
 
-    def _accuracy(self):
+    def accuracy(self):
         # ACCURACY
         self._acc_nd = tf.equal(self._pred_nd, tf.argmax(cnn._label_nd, 1))
 
+<<<<<<< HEAD:TensorFlow/face_classifier/simple_CNN_refactoring.py~
     def net_initializer(self):
         #self.simple_CNN(self._feature_nd)
         self.VGG16(self._feature_nd)
@@ -183,26 +136,25 @@ class CNN(netParam):
               .format(iter, np.sum(train_acc)/len(train_acc)*100, np.sum(eval_acc)/len(eval_acc)*100))
 
 '''
+=======
+cnn = CNN()
+    
+cnn.architecture(cnn._feature_nd)
+cnn.cost_function()
+cnn.optimizer()
+cnn.prediction()
+cnn.accuracy()
+
+>>>>>>> parent of 7fe4582... refactoring, splitting image into windows in progress:TensorFlow/face_classifier/Refactoring/simple_CNN_refactoring.py
 plt.ion()
 plt.close('all')
 fig_pred, ax_pred = plt.subplots(5, 5, figsize=(10, 10))
 
 plt.show()
-'''
 
-cnn = CNN()
-cnn.net_initializer()
-cnn.train()
-
-'''
 with tf.Session() as sess:
-    cnn = CNN()
-    cnn.net_initializer()
-    
     sess.run(tf.global_variables_initializer())
 
-
-    
     for iter in tqdm(range(cnn._epoch)):
         batch_index = np.random.choice(len(cnn._train_data), size=cnn._batch_size)
         batch_x = cnn._train_data[batch_index]
@@ -220,10 +172,8 @@ with tf.Session() as sess:
             print("({0}) TRAIN ACC.: {1} % | EVAL ACC.: {2} %"\
                   .format(iter, np.sum(train_acc)/len(train_acc)*100, np.sum(eval_acc)/len(eval_acc)*100))
 
-            #print(pred2[:10])
-            #print(sess.run(cnn._logits_nd, feed_dict={cnn._feature_nd: batch_x}))
-            #print(cnn._train_labels[:cnn._batch_size][:10])
-
+            #print(pred[:10])
+            
             for i in range(5):
                 for j in range(5):
                     if (i == 0) and (j==0):
@@ -244,7 +194,6 @@ with tf.Session() as sess:
             plt.tight_layout()
             plt.pause(0.0001)
             plt.draw()
-'''
 
 '''
 if __name__ == "__main__":
