154d429268713ce3274c6d09cf1177d74a5e1540
liprin1129 <liprin1129@gmail.com>
2019-01-24 17:18:49 +0900

    Tensorflow-hub: fastRCNN, face detection

---
+++ TensorFlow/Face-Recognition/detection_phase.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+import tensorflow_hub as hub
+import numpy as np
+#import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import cv2
+
+tf.reset_default_graph()
+
+img_placeholder = tf.placeholder(tf.string)
+
+filename = '/home/Downloads/lord_of_the_ring.jpg'
+#filename = '/home/Downloads/gollum.jpg'
+#filename = '/home/Downloads/Explorer_HD2K_SN19671_06-32-16.png'
+
+with tf.gfile.GFile(filename, 'rb') as fid:
+    image_data_binary = fid.read()
+
+#rgb_image = tf.image.decode_jpeg(img_placeholder, channels=3)
+rgb_image = tf.image.decode_png(img_placeholder, channels=3)
+rgb_image = tf.image.convert_image_dtype(rgb_image, tf.float32)
+resized_rgb_image = tf.image.resize_images(rgb_image, [128, 128])
+resized_rgb_image = tf.expand_dims(resized_rgb_image, 0)
+##########################################
+
+# Apply image detector on a single image.
+module = hub.Module("https://tfhub.dev/google/faster_rcnn/openimages_v4/inception_resnet_v2/1")
+detector_output = module(resized_rgb_image, as_dict=True)
+class_names = detector_output["detection_class_entities"]
+boxes = detector_output['detection_boxes']
+scores = detector_output["detection_scores"]
+
+##########################################
+with tf.Session() as sess:
+    #sess.run(tf.global_variables_initializer())
+    sess.run([tf.global_variables_initializer(), tf.tables_initializer()])
+    #image = sess.run(rgb_image, feed_dict={img_placeholder: image_data_binary})
+
+    #print(np.max(image))
+    origin_img, detected_class_names, detected_image_boxes, detected_scores = sess.run([rgb_image, class_names, boxes, scores],
+        feed_dict={img_placeholder: image_data_binary})
+    
+    shape = origin_img.shape
+    for ce, b, s in zip (detected_class_names, detected_image_boxes, detected_scores):
+        ce = ce.decode('ascii')
+        if (ce == "Human face" or ce == "Person") and s>0.5:
+            s1, s2, s3, s4 = int(b[1] * shape[1]), int(b[0] * shape[0]), int(b[3] * shape[1]), int(b[2] * shape[0])
+            cv2.rectangle(origin_img, (s1, s2), (s3, s4), (0, 255, 0), 5)
+            cv2.putText(origin_img, ce, (s1, s2), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            #print(s)
+            cv2.putText(origin_img, str(s), (s1, s2+30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            #print("< {0}: {1} >".format(ce, b))
+    
+    mpimg.imsave("/home/Downloads/detected.png", origin_img)
--- TensorFlow/Self-Practices/tensorflow-hub/TFHubSample-master/ImageClassification/my_test.py
+++ TensorFlow/Self-Practices/tensorflow-hub/TFHubSample-master/ImageClassification/my_test.py
@@ -16,7 +16,7 @@ params = {
 }
 
 dataset = Dataset(params)
-
+print("======> dataset.image_data: ", dataset.img_data)
 #module = hub.Module('https://tfhub.dev/google/imagenet/inception_v3/classification/1')
 #logits = module(dict(images=dataset.img_data))
 #print(logits)
@@ -24,17 +24,31 @@ dataset = Dataset(params)
 #softmax = tf.nn.softmax(logits)
 #top_predictions = tf.nn.top_k(softmax, top_k, name='top_predictions')
 
-module = hub.Module('https://tfhub.dev/google/imagenet/resnet_v2_50/feature_vector/1',
+module = hub.Module('https://tfhub.dev/google/imagenet/resnet_v1_50/classification/1',
                     trainable=True)   # Trainable is True since we are going to fine-tune the model
+print("\n========> output info dict:")
+[print('{0}: {1}'.format(k, v)) for k, v in sorted(module.get_output_info_dict(signature='image_classification').items())]
 
-print(dataset.img_data)
-
-module_features = module(dict(images=dataset.img_data), signature="image_feature_vector",
+module_features = module(dict(images=dataset.img_data), signature="image_classification",
                          as_dict=True)
-features = module_features["default"]
+#features = module_features["default"]
+features = module_features["resnet_v1_50/block2"]
 
-print(features)
+print("\n==========> features: ", features)
 
+config = tf.ConfigProto(allow_soft_placement=True)
+config.gpu_options.allow_growth = True
+config.gpu_options.per_process_gpu_memory_fraction = 0.9 # making sure Tensorflow doesn't overflow the GPU
+
+session = tf.InteractiveSession(config=config)
+
+if not os.path.exists('summaries'):
+    os.mkdir('summaries')
+if not os.path.exists(os.path.join('summaries','third')):
+    os.mkdir(os.path.join('summaries','third'))
+
+summ_writer = tf.summary.FileWriter(os.path.join('summaries','third'), session.graph)
+'''
 with tf.variable_scope('CustomLayer'):
     weight = tf.get_variable('weights', initializer=tf.truncated_normal((2048, n_class)))
     bias = tf.get_variable('bias', initializer=tf.ones((n_class)))
@@ -60,3 +74,4 @@ if not os.path.exists(os.path.join('summaries','second')):
     os.mkdir(os.path.join('summaries','second'))
 
 summ_writer = tf.summary.FileWriter(os.path.join('summaries','second'), session.graph)
+'''
