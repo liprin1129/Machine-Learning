Batch normalization bug fixed.
Note: the operations which update the moving_mean and moving_variance variables will not be added as dependencies of your training operation and so must be run separately. For example:
extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
sess.run([train_op, extra_update_ops], ...)

TensorFlow/Face-Recognition/facenet.py
@@ -14,13 +14,13 @@ def unit_conv_shortcut(_input_tensor, unit_num_int, _output_ch_list=[], _trainin
     def _repetitive_layers():
         with tf.variable_scope("conv1"):
             rep_conv = dlay.conv_layer_2d(_input_tensor, _output_ch_int=_output_ch_list[0], _kernel_w_int=3, _kernel_h_int=3, 
-                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm=True, _train_val_phase=_training)
+                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm_ph=_training)
         with tf.variable_scope("conv2"):
             rep_conv = dlay.conv_layer_2d(rep_conv, _output_ch_int=_output_ch_list[1], _kernel_w_int=3, _kernel_h_int=3, 
-                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm=True, _train_val_phase=_training)
+                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm_ph=_training)
         with tf.variable_scope("conv3"):
             rep_conv = dlay.conv_layer_2d(rep_conv, _output_ch_int=_output_ch_list[2], _kernel_w_int=3, _kernel_h_int=3, 
-                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm=True, _train_val_phase=_training)
+                                            _stride_list=[1, 1, 1, 1], _batch_norm_ph=_training)
         return rep_conv
 
     with tf.variable_scope("unit{0}".format(unit_num_int)):
@@ -28,7 +28,7 @@ def unit_conv_shortcut(_input_tensor, unit_num_int, _output_ch_list=[], _trainin
             #short_cut_tensor = tf.identity(conv)
             with tf.variable_scope("short_cut"):
                 short_cut_tensor = dlay.conv_layer_2d(_input_tensor, _output_ch_int=_output_ch_list[2], _kernel_w_int=3, _kernel_h_int=3,
-                                                        _stride_list=[1, 1, 1, 1], _batch_norm=True, _train_val_phase=_training)
+                                                        _stride_list=[1, 1, 1, 1], _batch_norm_ph=_training)
                 
                 #short_cut_tensor = tf.multiply(short_cut_tensor, 10)
 
@@ -46,13 +46,13 @@ def unit_conv_skip(_input_tensor, unit_num_int, _output_ch_list=[], _training=No
     def _repetitive_layers():
         with tf.variable_scope("conv1"):
             rep_conv = dlay.conv_layer_2d(_input_tensor, _output_ch_int=_output_ch_list[0], _kernel_w_int=3, _kernel_h_int=3, 
-                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm=True, _train_val_phase=_training)
+                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm_ph=_training)
         with tf.variable_scope("conv2"):
             rep_conv = dlay.conv_layer_2d(rep_conv, _output_ch_int=_output_ch_list[1], _kernel_w_int=3, _kernel_h_int=3, 
-                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm=True, _train_val_phase=_training)
+                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm_ph=_training)
         with tf.variable_scope("conv3"):
             rep_conv = dlay.conv_layer_2d(rep_conv, _output_ch_int=_output_ch_list[2], _kernel_w_int=3, _kernel_h_int=3, 
-                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm=True, _train_val_phase=_training)
+                                            _stride_list=[1, 1, 1, 1], _batch_norm_ph=_training)
         return rep_conv
 
     with tf.variable_scope("unit{0}".format(unit_num_int)):
@@ -60,7 +60,7 @@ def unit_conv_skip(_input_tensor, unit_num_int, _output_ch_list=[], _training=No
             #short_cut_tensor = tf.identity(conv)
             with tf.variable_scope("short_cut"):
                 short_cut_tensor = dlay.conv_layer_2d(_input_tensor, _output_ch_int=_output_ch_list[2], _kernel_w_int=3, _kernel_h_int=3,
-                                                        _stride_list=[1, 1, 1, 1], _batch_norm=True, _train_val_phase=_training)
+                                                        _stride_list=[1, 1, 1, 1], _batch_norm_ph=_training)
                 
                 #short_cut_tensor = tf.multiply(short_cut_tensor, 10)
 
@@ -78,13 +78,13 @@ def unit_conv_maxpool(_input_tensor, unit_num_int, _output_ch_list=[], _training
     def _repetitive_layers():
         with tf.variable_scope("conv1"):
             rep_conv = dlay.conv_layer_2d(_input_tensor, _output_ch_int=_output_ch_list[0], _kernel_w_int=3, _kernel_h_int=3, 
-                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm=True, _train_val_phase=_training)
+                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm_ph=_training)
         with tf.variable_scope("conv2"):
             rep_conv = dlay.conv_layer_2d(rep_conv, _output_ch_int=_output_ch_list[1], _kernel_w_int=3, _kernel_h_int=3, 
-                                            _stride_list=[1, 2, 2, 1], _activation_fn=tf.nn.relu, _batch_norm=True, _train_val_phase=_training)
+                                            _stride_list=[1, 2, 2, 1], _activation_fn=tf.nn.relu, _batch_norm_ph=_training)
         with tf.variable_scope("conv3"):
             rep_conv = dlay.conv_layer_2d(rep_conv, _output_ch_int=_output_ch_list[2], _kernel_w_int=3, _kernel_h_int=3, 
-                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm=True, _train_val_phase=_training)
+                                            _stride_list=[1, 1, 1, 1], _batch_norm_ph=_training)
         return rep_conv
 
     with tf.variable_scope("unit{0}".format(unit_num_int)):
@@ -119,7 +119,9 @@ def facenet(_input, train_validation_phase, outputs_logits_int, *layer_shape_dic
         conv = dlay.conv_layer_2d(_input, _output_ch_int=64, _kernel_w_int=3, _kernel_h_int=3,
                                         _stride_list=[1, 2, 2, 1],
                                         _pooling_window_shape=[2,2], _pooling_type = "MAX", 
-                                        _pooling_padding = "SAME", _pooling_strides = [2,2], _batch_norm=True, _train_val_phase=train_validation_phase)
+                                        _pooling_padding = "SAME", _pooling_strides = [2,2], 
+                                        _activation_fn=tf.nn.relu,
+                                        _batch_norm_ph=train_validation_phase)
         #print("\n===> ", conv)
 
     with tf.variable_scope("block1"):
@@ -179,8 +181,7 @@ def facenet(_input, train_validation_phase, outputs_logits_int, *layer_shape_dic
                             padding="VALID",
                             strides=[1,1])
         #logits = dlay.conv_layer_2d(logits, _output_ch_int=1001, _kernel_w_int=1, _kernel_h_int=1, _stride_list=[1, 1, 1, 1],
-        logits = dlay.conv_layer_2d(logits, _output_ch_int=outputs_logits_int, _kernel_w_int=1, _kernel_h_int=1, _stride_list=[1, 1, 1, 1],
-                                        _bias=True, _train_val_phase=train_validation_phase)
+        logits = dlay.conv_layer_2d(logits, _output_ch_int=outputs_logits_int, _kernel_w_int=1, _kernel_h_int=1, _stride_list=[1, 1, 1, 1], _bias=True)
         logits = tf.squeeze(logits)
     return logits
 
@@ -196,6 +197,7 @@ class HyperParameters(object):
         self.batch = _batch_size_int
         self.learning_rate = _learning_rate_int
         self.output = _num_outputs_int
+
 
 class FacenetModel(HyperParameters):
     def __init__(self, _num_epochs_int, _batch_size_int, _learning_rate_int, _num_outputs_int):
@@ -263,107 +265,3 @@ class FacenetModel(HyperParameters):
 
         _train_valid_iterator_setup()
         _facenet_architecture_setup()
-
-
-if __name__=="__main__":
-    epoch = 5
-    batch = 20
-    learning_rate = 0.001
-    num_outputs = 9
-
-    model = FacenetModel(epoch, batch, learning_rate, num_outputs)
-
-    model.block1["unit1"] = [64, 64, 256]
-    model.block1["unit2"] = [64, 64, 256]
-    model.block1["unit3"] = [64, 64, 256]
-    
-    model.block2["unit1"] = [128, 128, 512]
-    model.block2["unit2"] = [128, 128, 512]
-    model.block2["unit3"] = [128, 128, 512]
-    model.block2["unit4"] = [128, 128, 512]
-
-    model.block3["unit1"] = [256, 256, 1024]
-    model.block3["unit2"] = [256, 256, 1024]
-    model.block3["unit3"] = [256, 256, 1024]
-    model.block3["unit4"] = [256, 256, 1024]
-    model.block3["unit5"] = [256, 256, 1024]
-    model.block3["unit6"] = [256, 256, 1024]
-
-    model.block4["unit1"] = [512, 512, 2048]
-    model.block4["unit2"] = [512, 512, 2048]
-    model.block4["unit3"] = [512, 512, 2048]
-
-    model('/home/shared-data/SJC_Dev/Projects/SJC_Git/Face-Detector/SJC-Face-Data/', 224, 224)
-
-    if not os.path.exists('summaries'):
-        os.mkdir('summaries')
-    if not os.path.exists(os.path.join('summaries','facenet')):
-        os.mkdir(os.path.join('summaries','face'))
-
-    config_proto = tf.ConfigProto()
-    config_proto.gpu_options.allow_growth = True
-    #config_proto.gpu_options.per_process_gpu_memory_fraction = 0.9
-    with tf.Session(config=config_proto) as sess:
-        summ_writer = tf.summary.FileWriter(os.path.join('summaries','facenet'), sess.graph)
-        sess.run(tf.global_variables_initializer())
-
-        training_handle = sess.run(model.train_iterator.string_handle())
-        validation_handle = sess.run(model.validation_iterator.string_handle())
-        for _ in tqdm(range(epoch)):
-            try:
-                print("=== Training ==="); sys.stdout.flush()
-                sess.run(model.train_iterator.initializer)
-                total_accuracy = 0
-                count = 0
-                print("Processing: ", end=""); sys.stdout.flush()
-                while True:
-                    count += 1
-                    print(".", end=""); sys.stdout.flush()
-
-                    extracted_data = sess.run(model.get_next_in_interators, feed_dict={model.handle_placeholder: training_handle})
-                    _ = sess.run(model.optimizer, feed_dict={model.input_dataset_placeholder:extracted_data[0], model.labels_dataset_placeholder:extracted_data[1], model.train_valid_placeholder:True})
-
-                    pred = sess.run(model.predictions, feed_dict={model.input_dataset_placeholder:extracted_data[0], model.train_valid_placeholder:True})
-                    total_accuracy += np.sum(np.equal(pred, extracted_data[1])) / batch
-
-                    #print("{0} accuracy: {1}".format(np.sum(equality), equality)); sys.stdout.flush()
-                    #print("expected:\t{0} \npredicted:\t{1}".format(extracted_data[1], pred)); sys.stdout.flush()
-            except tf.errors.OutOfRangeError:
-                print("\n{0} %".format((total_accuracy/count)*100)); sys.stdout.flush()
-                pass
-
-            try:
-                print("=== Validation ==="); sys.stdout.flush()
-                sess.run(model.validation_iterator.initializer)
-                total_accuracy = 0
-                count = 0
-                print("Processing: ", end=""); sys.stdout.flush()
-                while True:
-                    count += 1
-                    print(".", end=""); sys.stdout.flush()
-
-                    extracted_data = sess.run(model.get_next_in_interators, feed_dict={model.handle_placeholder: validation_handle})
-                    pred = sess.run(model.predictions, feed_dict={model.input_dataset_placeholder:extracted_data[0], model.train_valid_placeholder:True})
-                    total_accuracy += np.sum(np.equal(pred, extracted_data[1])) / batch
-
-                    #equality = np.equal(pred, extracted_data[1])
-                    #print("{0} accuracy: {1}".format(np.sum(equality), equality)); sys.stdout.flush()
-                    #print("expected:\t{0} \npredicted:\t{1}".format(extracted_data[1], pred)); sys.stdout.flush()
-            except tf.errors.OutOfRangeError:
-                print("\n{0} %".format((total_accuracy/count)*100)); sys.stdout.flush()
-                pass
-
-
-    """
-    print(conv)
-
-    print("\n====> Global variable: ")
-    [print(i) for i in tf.global_variables()]
-
-    print("\n====> Local variable: ")
-    [print(i) for i in tf.local_variables()]
-
-    print("\n====> Trainable variable: ")
-    [print(i) for i in tf.trainable_variables()]
-    """
-

TensorFlow/Face-Recognition/recognition_phase.py
@@ -4,23 +4,15 @@ import tensorflow as tf
 import sys
 import numpy as np
 from tqdm import tqdm
+import tensorboard_helper as tbh
 
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('image_path', '/home/shared-data/SJC_Dev/Projects/SJC_Git/Face-Detector/SJC-Face-Data/', "root directory including images and tfrecords")
 
-data_dir_root_str = FLAGS.image_path
-data_image_dir_str = os.path.join(data_dir_root_str, 'images')
 
-# Image folder list which is used to identify the number of net's output logits
-data_image_list = os.listdir(data_image_dir_str)
-data_image_list = [os.path.join(data_image_dir_str, x) for x in data_image_list]
-data_image_list = [os.path.isdir(x) for x in data_image_list]
+# Set tensorboard folder name
+summary_name_str = 'facenet2'
 
-# Create a folder to save tensorboard data
-if not os.path.exists('summaries'):
-    os.mkdir('summaries')
-if not os.path.exists(os.path.join('summaries','facenet')):
-    os.mkdir(os.path.join('summaries','facenet'))
 
 """ ################
 # Hyper parameters #
@@ -29,7 +21,9 @@ epoch = 60
 batch = 50
 learning_rate = 0.001
 
-num_outputs = sum(data_image_list)
+num_outputs = len(list(
+    filter(lambda y: os.path.isdir(y), list(
+        map(lambda x: os.path.join(FLAGS.image_path+"images", x), os.listdir(FLAGS.image_path+"images"))))))
 
 model = FacenetModel(epoch, batch, learning_rate, num_outputs) # Initialize FacenetModel class to set shapes of layers
 
@@ -53,34 +47,9 @@ model.block4["unit1"] = [512, 512, 256]
 model.block4["unit2"] = [512, 512, 256]
 model.block4["unit3"] = [512, 512, 256]
 
-with tf.name_scope('training_performance'):
-    # Summaries need to be displayed
-    # Whenever you need to record the loss, feed the mean loss to this placeholder
-    train_loss_placeholder = tf.placeholder(tf.float32,shape=None,name='loss_summary')
-    # Create a scalar summary object for the loss so it can be displayed
-    train_loss_summary = tf.summary.scalar('loss', train_loss_placeholder)
-
-    # Whenever you need to record the loss, feed the mean test accuracy to this placeholder
-    train_accuracy_placeholder = tf.placeholder(tf.float32,shape=None, name='accuracy_summary')
-    # Create a scalar summary object for the accuracy so it can be displayed
-    train_accuracy_summary = tf.summary.scalar('accuracy', train_accuracy_placeholder)
-
-    # Merge all summaries together
-    train_performance_summaries = tf.summary.merge([train_loss_summary, train_accuracy_summary])
-
-with tf.name_scope('validation_performance'):
-    # Summaries need to be displayed
-
-    # Whenever you need to record the loss, feed the mean test accuracy to this placeholder
-    valid_accuracy_placeholder = tf.placeholder(tf.float32,shape=None, name='accuracy_summary')
-    # Create a scalar summary object for the accuracy so it can be displayed
-    valid_accuracy_summary = tf.summary.scalar('accuracy', valid_accuracy_placeholder)
-
-    # Merge all summaries together
-    valid_performance_summaries = tf.summary.merge([valid_accuracy_summary])
 
 def training():
-    model(data_dir_root_str, 224, 224)
+    model(FLAGS.image_path, 224, 224)
 
     # Add ops to save and restore all the variables.
     saver = tf.train.Saver()
@@ -90,8 +59,9 @@ def training():
     config_proto.gpu_options.per_process_gpu_memory_fraction = 0.8
     with tf.Session(config=config_proto) as sess:
 
-        summary_writer = tf.summary.FileWriter(os.path.join('summaries','facenet'), sess.graph) # Write tensorboard
-        
+        #summary_writer = tf.summary.FileWriter(os.path.join('summaries','facenet'), sess.graph) # Write tensorboard
+        summary_writer = tbh.summary_writer_fn(summary_name_str, sess)
+
         sess.run(tf.global_variables_initializer())
 
         training_handle = sess.run(model.train_iterator.string_handle())
@@ -110,7 +80,8 @@ def training():
                     extracted_data = sess.run(model.get_next_in_interators, feed_dict={model.handle_placeholder: training_handle})
                     
                     if len(extracted_data[1]) > 1:
-                        loss, _ = sess.run([model.loss, model.loss_minimizer], feed_dict={model.input_dataset_placeholder:extracted_data[0], model.labels_dataset_placeholder:extracted_data[1], model.train_valid_placeholder:True})
+                        extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+                        loss, _, _ = sess.run([model.loss, model.loss_minimizer, extra_update_ops], feed_dict={model.input_dataset_placeholder:extracted_data[0], model.labels_dataset_placeholder:extracted_data[1], model.train_valid_placeholder:True})
                         pred = sess.run(model.predictions, feed_dict={model.input_dataset_placeholder:extracted_data[0], model.train_valid_placeholder:True})
                         total_accuracy += np.sum(np.equal(pred, extracted_data[1])) / batch
 
@@ -118,10 +89,7 @@ def training():
                     #print("expected:\t{0} \npredicted:\t{1}".format(extracted_data[1], pred)); sys.stdout.flush()
             except tf.errors.OutOfRangeError:
                 # Execute the summaries defined above
-                train_summary = sess.run(train_performance_summaries, feed_dict={train_loss_placeholder:loss, train_accuracy_placeholder:(total_accuracy/count)*100})
-                # Write the obtained summaries to the file, so it can be displayed in the TensorBoard
-                summary_writer.add_summary(train_summary, current_epoch_int)
-                summary_writer.flush()
+                tbh.write_loss_and_accuracy_summary_fn(summary_writer, sess, loss, total_accuracy, count, epoch)
 
                 print("\n{0} %".format((total_accuracy/count)*100)); sys.stdout.flush()
                 pass
@@ -139,17 +107,14 @@ def training():
                     extracted_data = sess.run(model.get_next_in_interators, feed_dict={model.handle_placeholder: validation_handle})
 
                     if len(extracted_data[1]) > 1:
-                        pred = sess.run(model.predictions, feed_dict={model.input_dataset_placeholder:extracted_data[0], model.train_valid_placeholder:True})
+                        pred = sess.run(model.predictions, feed_dict={model.input_dataset_placeholder:extracted_data[0], model.train_valid_placeholder:False})
                         total_accuracy += np.sum(np.equal(pred, extracted_data[1])) / batch
 
             except tf.errors.OutOfRangeError:
                 print("\n{0} %".format((total_accuracy/count)*100)); sys.stdout.flush()
 
                 # Execute the summaries defined above
-                valid_summary = sess.run(valid_performance_summaries, feed_dict={valid_accuracy_placeholder:(total_accuracy/count)*100})
-                # Write the obtained summaries to the file, so it can be displayed in the TensorBoard
-                summary_writer.add_summary(valid_summary, current_epoch_int)
-                summary_writer.flush()
+                tbh.write_accuracy(summary_writer, sess, total_accuracy, count, epoch)
 
                 # Save the variables to disk.
                 save_path = saver.save(sess, '/tmp/%s_%03d-of-%03d.ckpt' % ("facenet", current_epoch_int, epoch))
@@ -158,7 +123,10 @@ def training():
 
                 pass
 
-            
+def testing():
+     model(FLAGS.image_path, 224, 224)
+
+     
     
 def main(argv):
     training()

TensorFlow/Face-Recognition/tensorflow_deeplay.py
@@ -6,8 +6,7 @@ def conv_layer_2d(_input_tensor,
                     _bias=None, _activation_fn=None, 
                     _pooling_window_shape=None, _pooling_type = None, 
                     _pooling_padding = None, _pooling_strides = None,
-                    _batch_norm = None,
-                    _train_val_phase = None):
+                    _batch_norm_ph = None):
     """
     Create compact convolutional layer by combining tf.nn.conv2d, bias, activation, and pooling functions.
     
@@ -39,8 +38,7 @@ def conv_layer_2d(_input_tensor,
                                 _input_tensor.get_shape()[3], _output_ch_int], 
                             dtype=tf.float32, 
                             initializer=tf.random_normal_initializer(), 
-                            regularizer=tf.contrib.layers.l2_regularizer(scale=0.1), 
-                            trainable=True)
+                            regularizer=tf.contrib.layers.l2_regularizer(scale=0.1))
 
     # Convolutional node
     with tf.variable_scope("conv2d"):
@@ -52,13 +50,12 @@ def conv_layer_2d(_input_tensor,
             bias_var = tf.get_variable("bias", 
                                         _output_ch_int, 
                                         dtype=tf.float32, 
-                                        initializer=tf.random_normal_initializer(), 
-                                        trainable=True)
+                                        initializer=tf.random_normal_initializer())
             conv = tf.nn.bias_add(conv, bias_var)
 
-    if _batch_norm is not None:
+    if _batch_norm_ph is not None:
         with tf.variable_scope("batch_norm"):
-            conv = tf.layers.batch_normalization(conv, training=_train_val_phase)
+            conv = tf.layers.batch_normalization(conv, training=_batch_norm_ph)
 
     if _activation_fn is not None:
         with tf.variable_scope("activation"):
@@ -78,100 +75,3 @@ def conv_layer_2d(_input_tensor,
                             strides=_pooling_dict["_strides"])"""
 
     return conv
-
-"""
-def unit_conv_shortcut(_input_tensor, unit_num_int, _output_ch_list=[], _training=None):
-    assert _training is not None
-
-    def _repetitive_layers():
-        with tf.variable_scope("conv1"):
-            rep_conv = conv_layer_2d(_input_tensor, _output_ch_int=_output_ch_list[0], _kernel_w_int=3, _kernel_h_int=3, 
-                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm=True, _train_val_phase=_training)
-        with tf.variable_scope("conv2"):
-            rep_conv = conv_layer_2d(rep_conv, _output_ch_int=_output_ch_list[1], _kernel_w_int=3, _kernel_h_int=3, 
-                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm=True, _train_val_phase=_training)
-        with tf.variable_scope("conv3"):
-            rep_conv = conv_layer_2d(rep_conv, _output_ch_int=_output_ch_list[2], _kernel_w_int=3, _kernel_h_int=3, 
-                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm=True, _train_val_phase=_training)
-        return rep_conv
-
-    with tf.variable_scope("unit{0}".format(unit_num_int)):
-        with tf.variable_scope("bottlenet_v1"):
-            #short_cut_tensor = tf.identity(conv)
-            with tf.variable_scope("short_cut"):
-                short_cut_tensor = conv_layer_2d(_input_tensor, _output_ch_int=_output_ch_list[2], _kernel_w_int=3, _kernel_h_int=3,
-                                                        _stride_list=[1, 1, 1, 1], _batch_norm=True, _train_val_phase=_training)
-                
-                #short_cut_tensor = tf.multiply(short_cut_tensor, 0.1)
-
-            #conv = dlay.unit_conv(conv, _output_ch_list=[64, 64, 256], _training=train_phase)
-            rep_conv = _repetitive_layers()
-
-            bottle_neck = tf.nn.relu(tf.add(short_cut_tensor, rep_conv))
-
-    return bottle_neck
-
-def unit_conv_skip(_input_tensor, unit_num_int, _output_ch_list=[], _training=None):
-    assert _training is not None
-
-    def _repetitive_layers():
-        with tf.variable_scope("conv1"):
-            rep_conv = conv_layer_2d(_input_tensor, _output_ch_int=_output_ch_list[0], _kernel_w_int=3, _kernel_h_int=3, 
-                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm=True, _train_val_phase=_training)
-        with tf.variable_scope("conv2"):
-            rep_conv = conv_layer_2d(rep_conv, _output_ch_int=_output_ch_list[1], _kernel_w_int=3, _kernel_h_int=3, 
-                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm=True, _train_val_phase=_training)
-        with tf.variable_scope("conv3"):
-            rep_conv = conv_layer_2d(rep_conv, _output_ch_int=_output_ch_list[2], _kernel_w_int=3, _kernel_h_int=3, 
-                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm=True, _train_val_phase=_training)
-        return rep_conv
-
-    with tf.variable_scope("unit{0}".format(unit_num_int)):
-        with tf.variable_scope("bottlenet_v1"):
-            #short_cut_tensor = tf.identity(conv)
-            with tf.variable_scope("short_cut"):
-                short_cut_tensor = conv_layer_2d(_input_tensor, _output_ch_int=_output_ch_list[2], _kernel_w_int=3, _kernel_h_int=3,
-                                                        _stride_list=[1, 1, 1, 1], _batch_norm=True, _train_val_phase=_training)
-                
-                #short_cut_tensor = tf.multiply(short_cut_tensor, 0.1)
-
-            #conv = dlay.unit_conv(conv, _output_ch_list=[64, 64, 256], _training=train_phase)
-            rep_conv = _repetitive_layers()
-
-            bottle_neck = tf.nn.relu(tf.add(short_cut_tensor, rep_conv))
-
-    return bottle_neck
-
-def unit_conv_maxpool(_input_tensor, unit_num_int, _output_ch_list=[], _training=None):
-    assert _training is not None
-
-    def _repetitive_layers():
-        with tf.variable_scope("conv1"):
-            rep_conv = conv_layer_2d(_input_tensor, _output_ch_int=_output_ch_list[0], _kernel_w_int=3, _kernel_h_int=3, 
-                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm=True, _train_val_phase=_training)
-        with tf.variable_scope("conv2"):
-            rep_conv = conv_layer_2d(rep_conv, _output_ch_int=_output_ch_list[1], _kernel_w_int=3, _kernel_h_int=3, 
-                                            _stride_list=[1, 2, 2, 1], _activation_fn=tf.nn.relu, _batch_norm=True, _train_val_phase=_training)
-        with tf.variable_scope("conv3"):
-            rep_conv = conv_layer_2d(rep_conv, _output_ch_int=_output_ch_list[2], _kernel_w_int=3, _kernel_h_int=3, 
-                                            _stride_list=[1, 1, 1, 1], _activation_fn=tf.nn.relu, _batch_norm=True, _train_val_phase=_training)
-        return rep_conv
-
-    with tf.variable_scope("unit{0}".format(unit_num_int)):
-        with tf.variable_scope("bottlenet_v1"):
-            #short_cut_tensor = tf.identity(conv)
-            with tf.variable_scope("short_cut"):
-                short_cut_tensor = tf.nn.pool(_input_tensor, 
-                                                window_shape=[2,2], 
-                                                pooling_type="MAX",
-                                                padding="VALID",
-                                                strides=[2,2])
-                #short_cut_tensor = tf.multiply(short_cut_tensor, 0.1)
-
-            #conv = dlay.unit_conv(conv, _output_ch_list=[64, 64, 256], _training=train_phase)
-            rep_conv = _repetitive_layers()
-
-            bottle_neck = tf.nn.relu(tf.add(short_cut_tensor, rep_conv))
-
-    return bottle_neck
-"""