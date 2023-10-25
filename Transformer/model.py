b8f48e83a75012a3da64beda78711f944afc457f
Seongmuk Gang <purebuntu@PURE-Razer>
2023-10-26 01:47:46 +0900

    pytorch test

--- Transformer/model.py
+++ Transformer/model.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import math
 
+#print(torch.cuda.is_available())
+
 class InputEmbeddings(nn.Module):
 
     def __init__(self, d_model: int, vocab_size: int):
@@ -25,4 +27,3 @@ class PositionalEncoding(nn.Module):
         pe = torch.zeros(seq_len, d_model)
         # Create a vector of shape ( Seq_Len, 1)
         position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
-        
