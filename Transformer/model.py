84c8b2ccd366c36450d63dfee3ba06ed99ef7508
Seongmuk Gang <purebuntu@PURE-Razer>
2023-10-26 00:21:00 +0900

    transformer added

---
+++ Transformer/model.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import math
+
+class InputEmbeddings(nn.Module):
+
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+    
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        # Create a matri of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+        # Create a vector of shape ( Seq_Len, 1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        
